77bd5778ec79dac3b88210e11b21ffb307c5d8dd
xtelix <xtelix12@gmail.com>
2019-02-20 09:54:39 +0100

    firstCommit

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -0,0 +1,27 @@
+package com.cifpcarballeira.katas.FizzBuzz.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dam205
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+}
